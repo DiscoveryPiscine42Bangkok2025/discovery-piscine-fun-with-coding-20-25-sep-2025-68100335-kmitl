f0217a0bfca0653299ec389aebea9d710acc4fd0
68100335-kmitl <68100335@kmitl.ac.th>
2025-09-22 18:50:56 +0700

    Update build.sh

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,9 +1,13 @@
-if [ $# -eq 0 ]
-then
- echo "No arguments supplied"
-else
- for i in "$@"
- do
-  mkdir "ex$i"
- done
+#!/bin/bash
+
+# ถ้าไม่มี argument ให้แสดงข้อความ
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
 fi
+
+# วนลูปสร้างโฟลเดอร์
+for arg in "$@"
+do
+    mkdir -p "ex$arg"
+done
